32c9e4fdde8421529e6577ad82989187ae6f2967
coccoto <soprano.lune@gmail.com>
2024-01-21 22:52:11 +0900

    2024-01-21 22:52:11

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom/client';
 import App from '@/scripts/index'
+import theme from '@/scripts/theme'
 // material
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 
 const elementApp: HTMLElement | null = document.getElementById('app')
 
@@ -11,8 +13,8 @@ if (elementApp === null) {
 }
 const root = ReactDom.createRoot(elementApp);
 root.render(
-    <div>
+    <ThemeProvider theme={theme}>
         <CssBaseline></CssBaseline>
         <App></App>
-    </div>
+    </ThemeProvider>
 )
---
+++ client/src/scripts/theme.tsx
@@ -0,0 +1,9 @@
+// material
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+    typography: {
+        fontFamily: '"M PLUS Rounded 1c", sans-serif',
+    },
+})
+export default theme
