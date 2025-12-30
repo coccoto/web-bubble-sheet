781baaec1650482fc566594d55a1286280f5ec68
coccoto <soprano.lune@gmail.com>
2025-12-30 17:57:28 +0900

    2025-12-30 17:57:28

--- server/src/services/MenuListService.js
+++ server/src/services/MenuListService.js
@@ -8,8 +8,8 @@ module.exports = class {
     }
 
     async getViewMenu() {
-        const sqlPath = path.join(__dirname, '../sql/selectMenuList.sql')
-        const query = await this.dbManager.readFile(sqlPath)
+        const queryPath = path.join(__dirname, '../queries/selectMenuList.sql')
+        const query = await this.dbManager.readFile(queryPath)
         const result = await this.dbManager.select(query)
         const viewMenuModels = result.map(record => new ViewMenuModel(record))
         return viewMenuModels
