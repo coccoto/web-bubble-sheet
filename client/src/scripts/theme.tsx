4a8d8a9f672cf25c9edb763dd8fa4455a8c4fa27
coccoto <soprano.lune@gmail.com>
2025-12-31 05:24:27 +0900

    2025-12-31 05:24:27

--- client/src/scripts/theme.tsx
+++ client/src/scripts/theme.tsx
@@ -5,5 +5,10 @@ const theme = createTheme({
     typography: {
         fontFamily: '"M PLUS Rounded 1c", sans-serif',
     },
+    palette: {
+        background: {
+            default: '#fafafa',
+        },
+    },
 })
 export default theme
