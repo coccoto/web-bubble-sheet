d204e98ca6396155d2041d0057f83878f189a7ac
coccoto <soprano.lune@gmail.com>
2025-12-30 17:20:28 +0900

    2025-12-30 17:20:28

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 const Express = require('express')
 const app = Express()
 
-// Set up routes
-const Router = require(ROOT + '/server/routes/index')
-Router(app)
-
-// Serve static files
-app.use(Express.static(ROOT + '/client/dist'))
-
 // Enable JSON parsing
 app.use(Express.json())
 
 // Enable URL-encoded data parsing
 app.use(Express.urlencoded({ extended: true }))
+
+// Serve static files
+app.use(Express.static(ROOT + '/client/dist'))
+
+// Set up routes
+const Router = require(ROOT + '/server/src/routes')
+Router(app)
 
 module.exports = app
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,22 +1,21 @@
 const { logger } = require('@coccoto/node-logmanager')
-const MenuListController = require(ROOT + '/server/src/controllers/MenuListController')
+const MenuListController = require('../controllers/MenuListController')
 
 module.exports = (app) => {
-    
+
     app.route(['/']).get((req, res) => {
         res.sendFile(ROOT + '/client/dist/index.html')
-        return
     })
 
     app.route(['/api/menu-list']).post(async (req, res) => {
-        const endpoint = req.url
         try {
             const menuListController = new MenuListController(req, res)
-            res = await menuListController.main()
-            return
+            await menuListController.main()
         } catch (error) {
-            logger.error(`Error in ${endpoint} route. Error: ${error.message}`)
-            return
-        } 
+            logger.error(`Error in ${req.url}: ${error.message}`)
+            if (!res.headersSent) {
+                res.status(500).json({ error: 'Internal Server Error' })
+            }
+        }
     })
 }
