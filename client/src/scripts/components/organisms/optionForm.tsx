db0c9f1f6dbe30dd7b1726a867f9d4c3542ce64a
coccoto <soprano.lune@gmail.com>
2025-12-31 01:36:25 +0900

    2025-12-31 01:36:25

--- client/src/scripts/components/organisms/optionForm.tsx
+++ client/src/scripts/components/organisms/optionForm.tsx
@@ -1,6 +1,5 @@
 // react
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 // atoms
 import InputNumber from '@/scripts/components/atoms/inputNumber'
 import Button from '@/scripts/components/atoms/button'
@@ -10,7 +9,7 @@ import Context from '@/scripts/contexts/context'
 import styles from '@/styles/components/organisms/optionForm.module.sass'
 
 export default ():React.JSX.Element  => {
-    const navigate = useNavigate()
+
     const context = React.useContext(Context.Context)
 
     const refInputText = [
@@ -18,7 +17,7 @@ export default ():React.JSX.Element  => {
         React.useRef<HTMLInputElement>(null)
     ]
 
-    const handleGenerate = (): void => {
+    const handleSubmit = (): void => {
         if (refInputText[0].current === null || refInputText[1].current === null) {
             console.error('error')
             return
@@ -33,35 +32,24 @@ export default ():React.JSX.Element  => {
         })
     }
 
-    const handleGrading = (): void => {
-        navigate('/grading')
-    }
-
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
-                <div className={styles.generateWrapper}>
-                    <div className={styles.inputWrapper}>
-                        <InputNumber
-                            ref={refInputText[0]}
-                            label={'QUESTIONS'}
-                            defaultValue={String(context.answerFormStatus.questionNum)}
-                        ></InputNumber>
-                        <InputNumber
-                            ref={refInputText[1]}
-                            label={'CHOICES'}
-                            defaultValue={String(context.answerFormStatus.bubbleNum)}
-                        ></InputNumber>
-                    </div>
-                    <div>
-                        <Button handleSubmit={handleGenerate}>
-                            GENERATE
-                        </Button>
-                    </div>
+                <div className={styles.inputWrapper}>
+                    <InputNumber
+                        ref={refInputText[0]}
+                        label={'QUESTIONS'}
+                        defaultValue={String(context.answerFormStatus.questionNum)}
+                    ></InputNumber>
+                    <InputNumber
+                        ref={refInputText[1]}
+                        label={'CHOICES'}
+                        defaultValue={String(context.answerFormStatus.bubbleNum)}
+                    ></InputNumber>
                 </div>
                 <div>
-                    <Button handleSubmit={handleGrading}>
-                        SELF GRADING
+                    <Button handleSubmit={handleSubmit}>
+                        GENERATE
                     </Button>
                 </div>
             </div>
